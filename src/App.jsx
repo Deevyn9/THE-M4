9c218ef4cb70052ab3bb8d67d6fef05d0f1eb643
Divine Obeten <divineobeten9@gmail.com>
2023-08-01 10:29:09 +0100

    creating animation for hero page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import Slide from "./components/Slide Screen/Slide";
 
 import "./App.css";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <>
       <Slide />
-      
+      <div className="bmw__logo">Logo</div>
+      <Hero />
     </>
   );
 }
