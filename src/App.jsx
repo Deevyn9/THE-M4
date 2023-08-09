8aa5fc0bd5c1804671568470f0bff2a7a7a1391b
Divine Obeten <divineobeten9@gmail.com>
2023-08-09 21:08:46 +0100

    adding background, logo Icon and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 import Slide from "./components/Slide Screen/Slide";
 import Transparent from "./components/Transparent/Transparent";
+import BMW from "./assets/bmw logo.png";
 
 import "./App.css";
 import Hero from "./components/Hero/Hero";
@@ -9,7 +10,9 @@ function App() {
   return (
     <div className="App">
       <Slide />
-      <div className="bmw__logo">Logo</div>
+      <div className="bmw__logo">
+        <img src={BMW} alt="bmw logo" />
+      </div>
       <Hero />
       <Transparent />
       <div className="div__scroll red"></div>
