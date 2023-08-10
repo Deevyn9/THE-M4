338471eddd8c672d70a94a8db2aa8f7defa2abb5
Divine Obeten <divineobeten9@gmail.com>
2023-08-10 22:10:20 +0100

    animating hero page

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,18 @@ import "./Hero.css";
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="header__text"></div>
+      <div className="middle-texts">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae
+          laboriosam iusto molestias rerum, quia recusandae perspiciatis maiores
+          nam dignissimos placeat?
+        </p>
+        <button>See More</button>
+      </div>
+
+      <div className="bottom-texts">
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
     </div>
   );
 };
